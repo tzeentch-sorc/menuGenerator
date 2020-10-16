4ca572b4dc1ee4a9a07e3832ceca9f653d204f3a
Artem Slepov <artemslepov01@gmail.com>
2020-10-16 21:40:51 +0300

    Create template

---
+++ project/src/main/java/netcrackerPractice/menuGenerator.java
@@ -0,0 +1,11 @@
+package netcrackerPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class menuGenerator {
+    public static void main(String[] args) {
+        SpringApplication.run(menuGenerator.class, args);
+    }
+}
