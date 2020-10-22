b70ab2f2aa273d7708b96313588ac1ef7c680566
tzeentch-sorc <T.T585@yandex.ru>
2020-10-22 15:58:34 +0300

    Extended sql password field

--- project/backend/src/main/resources/DBscripts/schema.sql
+++ project/backend/src/main/resources/DBscripts/schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS accounts
 (
     id serial PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(200) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL
 );
