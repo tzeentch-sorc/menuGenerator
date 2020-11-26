edf1abeb60b93c5897181088454e313a8a8c5c9e
tzeentch-sorc <T.T585@yandex.ru>
2020-11-26 14:04:45 +0300

    clean up

--- project/backend/src/main/java/edu/netcracker/menugenerator/MenuGenerator.java
+++ project/backend/src/main/java/edu/netcracker/menugenerator/MenuGenerator.java
@@ -1,17 +1,9 @@
 package edu.netcracker.menugenerator;
 
-import edu.netcracker.menugenerator.entity.Meal;
-import edu.netcracker.menugenerator.repository.MealRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
-import java.util.Optional;
 
 @SpringBootApplication
 public class MenuGenerator {
